e68d4701f1c709e98ee2979767ac68e68dd07c9e
DaniNEFFEX <151150204+DaniNEFFEX@users.noreply.github.com>
2023-12-24 16:32:41 +0300

    Add files via upload

---
+++ Циклический алгоритм 9 вариант/Циклический алгоритм 9 вариант/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Циклический_алгоритм_9_вариант
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число n = ");
+            int n = int.Parse(Console.ReadLine());
+            double S = 111 , A = 5;
+            for (int k = 1; k <= n - 1;k++)
+            {
+                S = S *(Math.Sqrt(A * ((k + 1) / k)) / Math.Sqrt(Math.Pow(7,k)));
+            }
+            Console.WriteLine();
+            Console.WriteLine($"S={S}");
+        }
+    }
+}
